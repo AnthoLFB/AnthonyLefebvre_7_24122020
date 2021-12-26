d6bef61769acfbf440d7f16f18f59b5c8f457229
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-12-27 00:20:44 +0100

    Ajout de la classe permettant de démarrer l'application. Dans cette classe on récupère le tableau de recettes, avec les différents objets, puis on le transmet aux vues

---
+++ scripts/js/app.js
@@ -0,0 +1,50 @@
+//Classe permettant d'initialiser la vue et le js necessaire
+class App
+{
+    constructor()
+    {
+        //Contiendra les recettes
+        this.recipes = [];
+
+        //Contiendra la vue à appeler
+        this.view = null;
+    }
+
+    init()
+    {
+        /* --- Récupération des recettes --- */
+        const recipesRepository = new RecipesRepository("recipes.json");
+
+        recipesRepository.findAll()
+        .then((recipes) => {
+            
+            this.recipes = recipes;
+
+            //Création des objets de type ingredient & ustensil
+            this.recipes.forEach(recipe => {
+                
+                let ingredients = [];
+                let ustensils = [];
+                
+                recipe.ingredients.forEach(ingredient => {
+                    ingredients.push(new Ingredient(ingredient.ingredient, ingredient.quantity, ingredient.unit));   
+                })
+
+                recipe.ustensils.forEach(ustensil => {
+                    ustensils.push(new Ustensil(ustensil));
+                })
+
+                recipe.ingredients = ingredients;
+                recipe.ustensils = ustensils;
+            });
+        })
+        .then(() => {
+            this.view = new HomeView(this);
+        })
+        .then(() => {
+            this.view.render();
+        });
+    }
+}
+
+(new App()).init();
