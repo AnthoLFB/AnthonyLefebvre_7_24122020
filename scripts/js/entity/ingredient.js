4494292bde13292dec4188fabac3a6c8388bfbff
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-12-27 00:14:47 +0100

    Ajout de la classe Ingredient. Cette classe permet de créer des objets de type ingrédient via le tableau d'ingrédient récupéré avec la promesse

---
+++ scripts/js/entity/ingredient.js
@@ -0,0 +1,10 @@
+//Classe permettant de créer des objets de type ingredient
+class Ingredient
+{
+    constructor(name, quantity, unit)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+}
