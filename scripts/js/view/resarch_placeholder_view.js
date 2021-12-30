cb6ae86a9db0aed03edf4740555d4ba23ba144c9
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-12-30 21:56:07 +0100

    Ajout d'une fonction JS permettant de changer le texte du placeholder lors d'une recherche par ingrédient, appareil ou ustensile

--- scripts/js/view/resarch_placeholder_view.js
+++ scripts/js/view/resarch_placeholder_view.js
@@ -4,25 +4,22 @@ class ResearchPlaceholderView
     {
         //Champ ingrédient
         let searchByIngredientInput = document.getElementById('searchByIngredient');
-        searchByIngredientInput.addEventListener("mouseover", this.renamePlaceholder);
-        //searchByIngredientInput.addEventListener("mouseover", this.renamePlaceholder(searchByIngredientInput, "mon message"));
-        //searchByIngredientInput.addEventListener("mouseout", this.defaultPlaceholder(searchByIngredientInput, "message par default"));
+        searchByIngredientInput.addEventListener("mouseover", () => {this.renamePlaceholder(searchByIngredientInput, "Rechercher un ingrédient")});
+        searchByIngredientInput.addEventListener("mouseout", () => {this.renamePlaceholder(searchByIngredientInput, "Ingrédients")});
         
+               
         //Champ Appareil
-        //let searchByDeviceInput = document.getElementById('searchByDevice');
+        let searchByDeviceInput = document.getElementById('searchByDevice');
+        searchByDeviceInput.addEventListener("mouseover", () => {this.renamePlaceholder(searchByDeviceInput, "Rechercher un appareil")});
+        searchByDeviceInput.addEventListener("mouseout", () => {this.renamePlaceholder(searchByDeviceInput, "Appareils")});
         
         //Champ Ustensiles
-       // let searchByUtensilsInput = document.getElementById('searchByUtensils');
+        let searchByUtensilsInput = document.getElementById('searchByUtensils');
+        searchByUtensilsInput.addEventListener("mouseover", () => {this.renamePlaceholder(searchByUtensilsInput, "Rechercher un ustensile")});
+        searchByUtensilsInput.addEventListener("mouseout", () => {this.renamePlaceholder(searchByUtensilsInput, "Ustensiles")});
     }
     
-
-    renamePlaceholder()
-    {
-        console.log("test");
-        //input.setAttribute('placeholder',message);
-    };
-
-    defaultPlaceholder(input, message) 
+    renamePlaceholder(input, message)
     {
         input.setAttribute('placeholder', message);
     };
