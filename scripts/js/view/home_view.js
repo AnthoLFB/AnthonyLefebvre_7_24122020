691476ce7f1f29ebe7c985b2c48dff58036b2ba5
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-01-05 18:22:22 +0100

    Ajout de la classe permettant d'afficher les ustensiles

--- scripts/js/view/home_view.js
+++ scripts/js/view/home_view.js
@@ -3,6 +3,7 @@ import ResearchPlaceholderView from "./research_placeholder_view";
 import RecipeView from "./recipe_view";
 import IngredientsTagsView from "./ingredients_tags_view";
 import DevicesTagsView from "./devices_tags_view";
+import UstensilsTagsView from "./ustensils_tags_view";
 
 class HomeView
 {
@@ -18,6 +19,7 @@ class HomeView
         new RecipeView(this.app.recipes);
         new IngredientsTagsView(this.app.recipes);
         new DevicesTagsView(this.app.recipes);
+        new UstensilsTagsView(this.app.recipes);
     }
 }
 
