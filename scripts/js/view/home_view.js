4dae48e44ff5387d89447a91b0fd3e529098c980
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-01-05 17:51:56 +0100

    Appel de la nouvelle classe permettant d'afficher les tags ingrédients

--- scripts/js/view/home_view.js
+++ scripts/js/view/home_view.js
@@ -1,6 +1,7 @@
 /* Import des classes utilisées */
 import ResearchPlaceholderView from "./research_placeholder_view";
 import RecipeView from "./recipe_view";
+import IngredientsTagsView from "./ingredients_tags_view";
 
 class HomeView
 {
@@ -14,6 +15,7 @@ class HomeView
     {
         //Appel la vue et passe un tableau de recettes en paramètre
         new RecipeView(this.app.recipes);
+        new IngredientsTagsView(this.app.recipes);
     }
 }
 
