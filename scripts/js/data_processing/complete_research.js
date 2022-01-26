d4f6902061aa940fc9f9a678835132a598b2d3bc
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-01-26 10:54:31 +0100

    Ajout de la version en programmation native de l'algorithme de recherche.

--- scripts/js/data_processing/complete_research.js
+++ scripts/js/data_processing/complete_research.js
@@ -5,11 +5,34 @@ class CompleteResearch
     {
         this.userSearchValue = userSearchValue;
         this.recipes = recipes
+        this.filteredRecipes = [];
     }
 
     research()
     {
-        console.log("Filtrer les recettes en fonction de la recherche de l'utilisateur : " + this.userSearchValue);
+        for(let i = 0; i < this.recipes.length; i++)
+        {
+            if(this.recipes[i].name.toLowerCase().includes(this.userSearchValue.toLowerCase()))
+            {
+                this.filteredRecipes.push(this.recipes[i]);
+            }
+            else if(this.recipes[i].description.toLowerCase().includes(this.userSearchValue.toLowerCase()))
+            {
+                this.filteredRecipes.push(this.recipes[i]);
+            }
+            else
+            {
+                for (let j = 0; j < this.recipes[i].ingredients.length; j++)
+                {
+                    if(this.recipes[i].ingredients[j].name.toLowerCase().includes(this.userSearchValue.toLowerCase()))
+                    {
+                        this.filteredRecipes.push(this.recipes[i]);
+                    }
+                }
+            }
+        }
+
+        return this.filteredRecipes   
     }
 }
 
