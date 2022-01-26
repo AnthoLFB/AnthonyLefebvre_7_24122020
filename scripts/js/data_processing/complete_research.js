e5e3943365bac9ab38e4dfee8178526d012a3d88
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-01-26 10:35:06 +0100

    Mise en place d'un affichage générique afin de merge sur la branche principale

--- scripts/js/data_processing/complete_research.js
+++ scripts/js/data_processing/complete_research.js
@@ -5,34 +5,11 @@ class CompleteResearch
     {
         this.userSearchValue = userSearchValue;
         this.recipes = recipes
-        this.filteredRecipes = [];
     }
 
     research()
     {
-        for(let i = 0; i < this.recipes.length; i++)
-        {
-            if(this.recipes[i].name.toLowerCase().includes(this.userSearchValue.toLowerCase()))
-            {
-                this.filteredRecipes.push(this.recipes[i]);
-            }
-            else if(this.recipes[i].description.toLowerCase().includes(this.userSearchValue.toLowerCase()))
-            {
-                this.filteredRecipes.push(this.recipes[i]);
-            }
-            else
-            {
-                for (let j = 0; j < this.recipes[i].ingredients.length; j++)
-                {
-                    if(this.recipes[i].ingredients[j].name.toLowerCase().includes(this.userSearchValue.toLowerCase()))
-                    {
-                        this.filteredRecipes.push(this.recipes[i]);
-                    }
-                }
-            }
-        }
-
-        return this.filteredRecipes   
+        console.log("Filtrer les recettes en fonction de la recherche de l'utilisateur : " + this.userSearchValue);
     }
 }
 
